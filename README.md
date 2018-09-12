ac806b178494212895cae58e17a1111d53b09523
Boby Thomas <bobypt@gmail.com>
2018-09-12 10:17:54 +1000

    first draft.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Simplke game using node and redis
 
+## Build and run locally
+
+```docker-compose build```
+
+```docker-compose up```
+
+
+```curl -X PUT http://localhost:8080/api/v1/gameservice/score/red-count```
+
+```curl http://localhost:8080/api/v1/gameservice/score/red-count```
+
 
 
--- api/app.js
+++ api/app.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const uuidv1 = require('uuid/v1');
+var redis = require('redis');
+const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+var client = redis.createClient(REDIS_PORT, REDIS_HOST); 
 
 // Constants
 const PORT = 8080;
 const ID = uuidv1();
-const CONTEXT_BASE = "/api/v1/orderservice"
+const CONTEXT_BASE = "/api/v1/gameservice"
 
 // App
 const app = express();
@@ -22,8 +27,33 @@ app.get(CONTEXT_BASE+'/health', (req, res) => {
   res.json({instanceId : ID, version: "1", status: "OK"});
 });
 
-app.get(CONTEXT_BASE+'/orders/:id', (req, res) => {
-  res.json({id: req.params.id, description: "Order for Name"+ req.params.id});
+app.get(CONTEXT_BASE+'/score/:key', (req, res) => {
+  client.get(req.params.key, function (error, result) {
+    if (error) {
+        console.log(error);
+        res.status(400);
+        res.send('Invalid request');
+    }
+    res.json({key: req.params.key, value: result});
+  });
+});
+
+app.put(CONTEXT_BASE+'/score/:key', (req, res) => {
+  client.incr(req.params.key, function (error, result) {
+    if (error) {
+        console.log(error);
+        client.setnx(req.params.key, '1', redis.print);
+    }
+    res.json({key: req.params.key, value: result});
+  });
+});
+
+//redis
+client.on('connect', function() {
+  console.log('Redis client connected');
+});
+client.on('error', function (err) {
+  console.log('Something went wrong ' + err);
 });
 
 
